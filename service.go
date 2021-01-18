b1e815ed88b072860592f0e7049bef8d8e4e9471
Kavya Kandhway <kavya@furrble.com>
2021-01-18 22:34:53 +0530

    removed comments

--- service.go
+++ service.go
@@ -1,13 +1,3 @@
-// Copyright 2013 Mathias Monnerville and Anthony Baillard.
-// All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package cloudinary provides support for managing static assets
-// on the Cloudinary service.
-//
-// The Cloudinary service allows image and raw files management in
-// the cloud.
 package cloudinary
 
 import (
